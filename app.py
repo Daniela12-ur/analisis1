8e878a5a3c36e0a7554053694e8c89f7a3a8624b
Daniela12-ur <danielaurrealadino@gmail.com>
2025-03-12 02:30:32 +0000

    v2

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-import streamlit as st 
 
-st.write("Hello world")
+import streamlit as st 
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10,20),
+    columns=('col %d' % i for i in range(20)))
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a', 'b','c'])
+print(chart_data)
+st.line_chart(chart_data)
---
+++ mapa.py
@@ -0,0 +1,11 @@
+
+import streamlit as st 
+import numpy as np
+import pandas as pd
+
+
+map_data=pd.DataFrame(
+    np.random.randn(1000,2)/[50,50] + [6.25184, -75.56359],
+    columns=['lat', 'lon'])
+st.map(map_data)
+
